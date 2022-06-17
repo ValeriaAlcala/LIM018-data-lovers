31928373af714040b1b3ebb3627155b6a6ee0ae5
Valeria <valeria@MacBook-Pro-de-Valeria.local>
2022-06-17 14:00:50 -0500

    nuevos cambios

--- src/data.js
+++ src/data.js
@@ -1,9 +1,34 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
+/*export const example = () => {
   return 'example';
 };
 
 export const anotherExample = () => {
   return 'OMG';
-};
+};*/
+
+export const prueba= (data) => {
+  const filtroHuman=
+  data.films[0].people.filter(x=>x.specie=="Human");
+
+let imagenhuman = filtroHuman.map(x=>x.img);
+
+return imagenhuman
+}
+
+export const title=(data)=>{
+  const image=
+  data.flims[0].title.filter(x=>x);
+
+let title=image.map(x=>x.poster);
+  return title
+}
+
+/*export const searchName=(data)=>{
+  return data.filter((data)=>{
+    if (data.films[0].tittle.indexOf(str) = -1) return false;
+    return true;
+  })
+} */
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
-import { example } from './data.js';
+import {prueba} from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+viewImage();
+function viewImage(){   
+  for (let i=0 ; i<data.films.length ; i++){
+   
+   const frameImg = document.createElement('img');
+   const elementoPadre = document.getElementById("imagenes")
+
+   elementoPadre.appendChild(frameImg);
+   frameImg.setAttribute("src", data.films[i].poster);
+   frameImg.setAttribute("class","imagenes" );
+  }
+}
+console.log(data);
+
+console.log(prueba(data));
