5ef1a3c30456af479c66de7713adc84f16a8ba72
Valeria <valeria@MacBook-Pro-de-Valeria.local>
2022-06-20 15:50:06 -0500

    Primeros filtrados

--- src/data.js
+++ src/data.js
@@ -15,12 +15,48 @@ export const prueba= (data) => {
 let imagenhuman = filtroHuman.map(x=>x.img);
 
 return imagenhuman
-
-
 }
 
+//FILTRAR POR PRODUCTOR = ISAO TAKAHATA//
+export function producerIsao (data){
+  let producer1;
 
+  producer1=data.filter(p => p.producer === "Isao Takahata");
 
+return producer1;
+}
+
+export function producerHayao (data){
+  let producer2;
+
+  producer2=data.filter(p => p.producer === "Hayao Miyazaki");
+
+return producer2;
+}
+
+export function producerToru (data){
+  let producer3;
+
+  producer3=data.filter(p => p.producer === "Hayao Miyazaki");
+
+return producer3;
+}
+
+export function producerToshio (data){
+  let producer4;
+
+  producer4=data.filter(p => p.producer === "Toshio Suzuki");
+
+return producer4;
+}
+
+export function producerYoshiaki (data){
+  let producer5;
+
+  producer5=data.filter(p => p.producer === "Yoshiaki Nishimura");
+
+return producer5;
+}
 /*export const searchName=(data)=>{
   return data.filter((data)=>{
     if (data.films[0].tittle.indexOf(str) = -1) return false;
--- src/main.js
+++ src/main.js
@@ -1,37 +1,75 @@
-import {prueba} from './data.js';
+//import { styleDisabled } from 'htmlhint';//
+import {prueba,producerIsao,producerHayao,producerToru,producerToshio,producerYoshiaki} from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-viewImage();
-function viewImage(){   
-  for (let i=0 ; i<data.films.length ; i++){
-   
-   const frameImg = document.createElement('img');
-   const elementoPadre = document.getElementById("imagenes")
+//FILTRADO GENERAL
+const filmsData=data.films;
+// eslint-disable-next-line no-console
+console.log(filmsData);
 
-   elementoPadre.appendChild(frameImg);
-   frameImg.setAttribute("src", data.films[i].poster);
-   frameImg.setAttribute("class","imagenes" );
-  }
+const aboutStudio=document.getElementById("firstbutton");
+aboutStudio.addEventListener("click",aboutStudioPage);
+
+const films=document.getElementById("secondbutton");
+films.addEventListener("click",filmspage)
+
+function aboutStudioPage(){
+  document.getElementById("homepage").style.display="none";
+  document.getElementById("studiopage").style.display="block";
 }
+
+function filmspage(){
+  document.getElementById("homepage").style.display="none";
+  document.getElementById("filmspage").style.display="block";
+
+    viewImage();
+      function viewImage(){   
+        for (let i=0 ; i<data.films.length ; i++){
+   
+      const frameImg = document.createElement('img');
+      const elementoPadre = document.getElementById("imagenes")
+
+        elementoPadre.appendChild(frameImg);
+        frameImg.setAttribute("src", data.films[i].poster);
+        frameImg.setAttribute("class","imagenes" );
+        
+        }
+      }
+// eslint-disable-next-line no-console
 console.log(data);
 
-const imagenes=prueba(data);
-prueba1();
-function prueba1(){
+  const imagenes=prueba(data);
+    prueba1();
+      function prueba1(){
+        for(let i=0; i<imagenes.length; i++){
 
-  for(let i=0; i<imagenes.length; i++){
-    const nuevasImg=document.createElement('img');
-    const imagenPadre=document.getElementById("nuevasImg")
+      const nuevasImg=document.createElement('img');
+      const imagenPadre=document.getElementById("nuevasImg")
 
-    imagenPadre.appendChild(nuevasImg);
-    nuevasImg.setAttribute("src",imagenes[i]);
-    nuevasImg.setAttribute("class","nuevasImg")
-  }
+        imagenPadre.appendChild(nuevasImg);
+        nuevasImg.setAttribute("src",imagenes[i]);
+        nuevasImg.setAttribute("class","nuevasImg")
+        }
+      }
+//console.log(prueba(data));
 
 }
 
 
-console.log(prueba(data));
+const isao=producerIsao(filmsData);
+// eslint-disable-next-line no-console
+console.log(isao);
 
+const hayao=producerHayao(filmsData);
+console.log(hayao);
+
+const toru=producerToru(filmsData);
+console.log(toru);
+
+const toshio=producerToshio(filmsData);
+console.log(toshio);
+
+const yoshiaki=producerYoshiaki(filmsData);
+console.log(yoshiaki);
